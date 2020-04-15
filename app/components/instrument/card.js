272462fadd16a5ffb4a241bf684df96c22f665c1
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-15 21:48:08 +0200

    Make part templates sortable by drag-and-drop

--- app/components/instrument/card.js
+++ app/components/instrument/card.js
@@ -1,17 +1,48 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency-decorators';
+import { task, keepLatestTask } from 'ember-concurrency-decorators';
+import { all } from 'ember-concurrency';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class InstrumentCardComponent extends Component {
   @service store
 
+  @tracked templates = []
   @tracked part
   @tracked position
 
+  constructor() {
+    super(...arguments);
+    this.loadData.perform();
+  }
+
   get sortedTemplates() {
-    return this.args.model.templates.sortBy('position');
+    return this.templates.sortBy('position');
+  }
+
+  @task
+  *loadData() {
+    const templates = yield this.args.model.templates;
+    this.templates = templates.sortBy('position');
+  }
+
+  @keepLatestTask
+  *sortTemplates({ draggedItem, sourceList, sourceIndex, targetList, targetIndex }) {
+    if (sourceList === targetList && sourceIndex === targetIndex) return;
+
+    sourceList.removeAt(sourceIndex);
+    targetList.insertAt(targetIndex, draggedItem);
+
+    this.templates = targetList;
+
+    const prevPosition = targetIndex > 0 ? this.templates[targetIndex - 1].position : 0;
+    const lastIdx = this.templates.length - 1;
+    const nextPosition = targetIndex < lastIdx ? this.templates[targetIndex + 1].position : this.templates[lastIdx - 1].position + 2;
+    const position = (prevPosition + nextPosition)/2;
+
+    draggedItem.position = position;
+    yield draggedItem.save();
   }
 
   @task
