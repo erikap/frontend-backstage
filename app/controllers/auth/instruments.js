eec9c40f492fe033950ef490ebf8bf7c6aa82e59
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-18 21:36:14 +0200

    Add new instruments on top of the list

--- app/controllers/auth/instruments.js
+++ app/controllers/auth/instruments.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
+import calculatePosition from '../../utils/calculate-position';
 
 export default class AuthInstrumentsController extends Controller {
   @service store
@@ -12,10 +13,20 @@ export default class AuthInstrumentsController extends Controller {
   *addInstrument(event) {
     const instrument = this.store.createRecord('instrument', {
       label: this.instrumentLabel,
-      position: 0
+      position: this.model.length ? 0 : 100
     });
     yield instrument.save();
+
+    if (this.model.length) {
+      // recalculate position of the previous first element
+      // to make sure our new element appears first in the list
+      const firstInstrument = this.model.objectAt(0);
+      firstInstrument.position = calculatePosition([instrument, ...this.model.slice(0)], 1);
+      yield firstInstrument.save();
+    }
+
     this.send('refreshModel');
+
     this.instrumentLabel = null;
 
     event.preventDefault();
